dd5edcc58eb201e2a38e0eb1cad01446ddbb1ba9
Medik <steadyeddie101@hotmail.co.uk>
2026-08-25 18:24:38 +0100

    Update getCampaignDifficulty() docs to mention setting timing

--- M2TWEOP Code/M2TWEOP library/luaPlugin.cpp
+++ M2TWEOP Code/M2TWEOP library/luaPlugin.cpp
@@ -986,20 +986,26 @@ sol::state* luaPlugin::init(std::string& luaFilePath, std::string& modPath)
 	tables.M2TWEOP.set_function("getOptions2", &gameHelpers::getOptions2);
 	
 	/***
-	Get the campaign difficulty modifiers.
+	Get the campaign difficulty modifiers. Note: If making changes, make sure to re-apply in onPreFactionTurnStart
 	@function M2TWEOP.getCampaignDifficulty1
 	@treturn campaignDifficulty1 options
 	@usage
-		local modifiers = M2TWEOP.getCampaignDifficulty1();
+	function onPreFactionTurnStart(eventData)
+		local difficultyOptions1 = M2TWEOP.getCampaignDifficulty1();
+		difficultyOptions1.orderFromGrowth = 0
+	end
 	*/
 	tables.M2TWEOP.set_function("getCampaignDifficulty1", &campaignHelpers::getCampaignDifficulty1);
 	
 	/***
-	Get the campaign difficulty modifiers.
+	Get the campaign difficulty modifiers. Note: If making changes, make sure to re-apply in onPreFactionTurnStart
 	@function M2TWEOP.getCampaignDifficulty2
 	@treturn campaignDifficulty2 options
 	@usage
-		local modifiers = M2TWEOP.getCampaignDifficulty2();
+	function onPreFactionTurnStart(eventData)
+		local difficultyOptions2 = M2TWEOP.getCampaignDifficulty2();
+		difficultyOptions2.popGrowthBonusAi = 0
+	end
 	*/
 	tables.M2TWEOP.set_function("getCampaignDifficulty2", &campaignHelpers::getCampaignDifficulty2);
 	
